f5e4a41499d49bf8ec3ee5a1aafc4a4ef2eb8ff4
Phan Viet Tan <tanphan6061@gmail.com>
2020-11-18 03:31:20 +0700

    add all equipments api and fix some thing

--- controllers/equipment.controller.js
+++ controllers/equipment.controller.js
@@ -2,16 +2,21 @@ const status = require('http-status');
 
 const Equipment = require('../models/Equipment');
 
+module.exports.getEquipment = async (req, res) => {
+  const equipments = await Equipment.find({});
+  return res.json({ equipments });
+};
+
 module.exports.createEquipment = async (req, res) => {
   const { name, icon } = req.body;
 
   try {
-    const equiment = await Equipment.create({
+    const equipment = await Equipment.create({
       name,
       icon,
     });
 
-    return res.json({ equiment });
+    return res.json({ equipment });
   } catch (err) {
     return res.status(status.INTERNAL_SERVER_ERROR).json({
       message: err.message,
