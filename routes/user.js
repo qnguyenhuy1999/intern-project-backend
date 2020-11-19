a78f5fb6414a3fdfa0447d93856040b191abc064
Phan Viet Tan <tanphan6061@gmail.com>
2020-11-19 18:24:27 +0700

    fix something

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,16 @@ const controller = require('../controllers/user.controller');
 const validator = require('../middlewares/validators/user.validator');
 const authMiddleware = require('../middlewares/auth.middleware');
 
-router.get('/getAllUser', authMiddleware.isAdmin, controller.getAllUser);
+router.get(
+  '/getAllUser',
+  authMiddleware.requireAuth,
+  authMiddleware.isAdmin,
+  controller.getAllUser
+);
 
 router.post(
   '/updateRoleUser',
+  authMiddleware.requireAuth,
   authMiddleware.isAdmin,
   validator.updateRoleUser,
   controller.updateRoleUser
